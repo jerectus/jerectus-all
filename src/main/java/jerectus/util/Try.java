84234324e96884f548861581ea037bcc75898543
unknown <nakalele@gmail.com>
2019-05-31 07:39:29 +0900

    commi

--- src/main/java/jerectus/util/Try.java
+++ src/main/java/jerectus/util/Try.java
@@ -6,8 +6,6 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
-import jerectus.util.function.Param2;
-import jerectus.util.function.Param3;
 import jerectus.util.function.ThrowableBiConsumer;
 import jerectus.util.function.ThrowableBiFunction;
 import jerectus.util.function.ThrowableConsumer;
@@ -16,22 +14,8 @@ import jerectus.util.function.ThrowableRunnable;
 import jerectus.util.function.ThrowableSupplier;
 
 public class Try {
-    public static <T1, T2> Param2<T1, T2> param(T1 param1, T2 param2) {
-        return new Param2<T1, T2>(param1, param2);
-    }
-
-    public static <T1, T2, T3> Param3<T1, T2, T3> param(T1 param1, T2 param2, T3 param3) {
-        return new Param3<T1, T2, T3>(param1, param2, param3);
-    }
-
     public static <T, R> Function<T, R> with(ThrowableFunction<T, R> fn) {
-        return arg -> {
-            try {
-                return fn.apply(arg);
-            } catch (Exception e) {
-                throw Sys.asRuntimeException(e);
-            }
-        };
+        return fn;
     }
 
     public static <T, R> Function<T, R> with(ThrowableFunction<T, R> fn, BiFunction<Exception, T, R> onCatch) {
@@ -45,34 +29,22 @@ public class Try {
     }
 
     public static <T, U, R> BiFunction<T, U, R> with(ThrowableBiFunction<T, U, R> fn) {
-        return (arg1, arg2) -> {
-            try {
-                return fn.apply(arg1, arg2);
-            } catch (Exception e) {
-                throw Sys.asRuntimeException(e);
-            }
-        };
+        return fn;
     }
 
     public static <T, U, R> BiFunction<T, U, R> with(ThrowableBiFunction<T, U, R> fn,
-            BiFunction<Exception, Param2<T, U>, R> onCatch) {
+            BiFunction<Exception, Object[], R> onCatch) {
         return (arg1, arg2) -> {
             try {
                 return fn.apply(arg1, arg2);
             } catch (Exception e) {
-                return onCatch.apply(e, Try.param(arg1, arg2));
+                return onCatch.apply(e, new Object[] { arg1, arg2 });
             }
         };
     }
 
     public static <R> Supplier<R> with(ThrowableSupplier<R> fn) {
-        return () -> {
-            try {
-                return fn.get();
-            } catch (Exception e) {
-                throw Sys.asRuntimeException(e);
-            }
-        };
+        return fn;
     }
 
     public static <R> Supplier<R> with(ThrowableSupplier<R> fn, Function<Exception, R> onCatch) {
@@ -86,40 +58,30 @@ public class Try {
     }
 
     public static <T> Consumer<T> with(ThrowableConsumer<T> fn) {
-        return arg -> {
-            try {
-                fn.accept(arg);
-            } catch (Exception e) {
-                throw Sys.asRuntimeException(e);
-            }
-        };
+        return fn;
     }
 
     public static <T, U> BiConsumer<T, U> with(ThrowableBiConsumer<T, U> fn) {
-        return (arg1, arg2) -> {
-            try {
-                fn.accept(arg1, arg2);
-            } catch (Exception e) {
-                throw Sys.asRuntimeException(e);
-            }
-        };
+        return fn;
     }
 
     public static Runnable with(ThrowableRunnable fn) {
-        return () -> {
-            try {
-                fn.run();
-            } catch (Exception e) {
-                throw Sys.asRuntimeException(e);
-            }
-        };
+        return fn;
     }
 
     public static <T> T get(ThrowableSupplier<T> fn) {
-        return with(fn).get();
+        try {
+            return fn.getEx();
+        } catch (Exception e) {
+            throw Sys.asRuntimeException(e);
+        }
     }
 
     public static void run(ThrowableRunnable fn) {
-        with(fn).run();
+        try {
+            fn.runEx();
+        } catch (Exception e) {
+            throw Sys.asRuntimeException(e);
+        }
     }
 }
--- src/test/java/jerectus/util/TryTest.java
+++ src/test/java/jerectus/util/TryTest.java
@@ -1,10 +1,14 @@
 package jerectus.util;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class TryTest {
     @Test
     public void test1() {
-        System.out.println(Try.get(() -> this.getClass().getMethod("test1")));
+        Arrays.asList(1, 2, 3).forEach(Try.with(it -> {
+            TryTest.class.getMethod("test1");
+        }));
     }
 }
